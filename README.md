93ec73793c560f3f71a65e00f78245567b75da7f
Kristen Vinh <kristenvinh@gmail.com>
2022-09-06 15:10:46 -0400

    Question 2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Eight Week SQL Challenge
  
+I'm working on improving my SQL skills by starting the 8 Week SQL Challenge by Danny Ma.
+
+This repository contains all my documentation for the challenges that I'm working on, starting with Week 1 -- [Danny's Diner](https://8weeksqlchallenge.com/case-study-1/).
+
--- Week 1/Solution.sql
+++ Week 1/Solution.sql
@@ -2,8 +2,6 @@
    Case Study Questions
    --------------------*/
 
--- 1. What is the total amount each customer spent at the restaurant?
--- 2. How many days has each customer visited the restaurant?
 -- 3. What was the first item from the menu purchased by each customer?
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 -- 5. Which item was the most popular for each customer?
@@ -13,16 +11,20 @@
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
 
--- Query Question 1: 
+-- 1. What is the total amount each customer spent at the restaurant?
 SELECT 
 customer_id,
-sum(menu.price)
-
+sum(menu.price) as amount_spent
 FROM
     dannys_diner.sales
 LEFT OUTER JOIN
     dannys_diner.menu
 ON
     sales.product_id = menu.product_id
+GROUP BY customer_id
+
+-- 2. How many days has each customer visited the restaurant?
+SELECT customer_id, COUNT (DISTINCT order_date) as days_visited
+FROM sales
 GROUP BY customer_id
 
